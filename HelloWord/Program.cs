af14105edb134f686342ffb507701118342ff5d1
FelipePaimRoos <104603120+FelipePaimRoos@users.noreply.github.com>
2023-04-05 09:38:38 -0300

    Atualização da classe Main

--- HelloWord/Program.cs
+++ HelloWord/Program.cs
@@ -1,28 +1,34 @@
-﻿using System;
+using System;
 class HelloWorld {
   static void Main() {
-      
+
     Aluno aluno1 = new Aluno("Felipe", 19, 10);
-    
+
     Aluno aluno2 = new Aluno("teste", 20, 9);
 
     Aluno aluno3 = new Aluno("teste1", 25, 10);
 
-    
+
     Vetor vetores = new Vetor();
-    
+
     vetores.adicionarAluno(aluno1);
     vetores.adicionarAluno(aluno2);
     vetores.adicionarAluno(aluno3);
-    
+
     System.Console.Write("O tamanho atual é " +vetores.tamanhoAtual() +"\n");
-    
+
     System.Console.Write("É existente o aluno: "+vetores.contem(aluno1)+ "\n");
 
     System.Console.Write(vetores.cheio());
+    
+    vetores.remove(aluno1);
+    
+    System.Console.Write("É existente o aluno: "+vetores.contem(aluno1)+ "\n");
+    
+    System.Console.Write("O tamanho atual é " +vetores.tamanhoAtual() +"\n");
 
  //   System.Console.Write(vetores.relatorio());
 
-   
+
   }
 }
