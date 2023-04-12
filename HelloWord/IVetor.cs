bbf74ff632a52746ca135b36f9b45d319b7685e8
FelipePaimRoos <104603120+FelipePaimRoos@users.noreply.github.com>
2023-04-12 18:51:33 -0300

    atualização

--- HelloWord/IVetor.cs
+++ HelloWord/IVetor.cs
@@ -1,15 +1,11 @@
 public interface IVetor{
 
-     void adicionarAluno (Aluno aluno);
+    public void adicionarAluno (Aluno aluno);
 
-     int tamanhoAtual();
+    public int tamanhoAtual();
 
-     bool contem(Aluno aluno);
+    public bool contem(Aluno aluno);
 
-     bool cheio();
-    
-     void novoVetor();
-    
-     bool remove(Aluno aluno);
+    public bool cheio();
 
 }
