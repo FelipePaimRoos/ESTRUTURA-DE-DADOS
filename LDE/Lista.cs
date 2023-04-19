4638cc03472372ac3e75f2ca0995e3df3c9173e0
FelipePaimRoos <104603120+FelipePaimRoos@users.noreply.github.com>
2023-04-19 13:13:34 -0300

    Atividade 6

---
+++ LDE/Lista.cs
@@ -0,0 +1,10 @@
+interface Lista {
+    public void insereInicio(int info);
+    public void insereFim(int info);
+    public bool estahVazia();
+    public bool remove(int info);
+    public int tamanho();
+    void imprimirInicioFim();
+
+    void imprimirFimInicio();
+}
---
+++ LDE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class HelloWorld {
+  static void Main() {
+
+    Lista lista1 = new LDE();
+
+    lista1.insereInicio(5);
+    lista1.insereFim(7);
+
+    lista1.imprimirFimInicio();
+
+    lista1.imprimirInicioFim();
+
+  }
+}
