93659d8598afeeaca187198f7867c4c73ceee529
FelipePaimRoos <104603120+FelipePaimRoos@users.noreply.github.com>
2023-05-30 19:37:22 -0300

    Add files via upload

---
+++ Atividade 11/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String [] args){
+
+        FilaVet fila = new FilaVet(10);
+
+        fila.add(1);
+        fila.add(10);
+
+        fila.imprimirFila();
+    }
+    
+}
